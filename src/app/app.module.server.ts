ec73afca8a668933f0d3bd60ef95d1f1dd87a820
Natalia Erazo <n.erazog@uniandes.edu.co>
2025-05-19 16:52:19 -0500

    corecciones

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -3,11 +3,13 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { AerolineaModule } from './aerolinea/aerolinea.module';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    AerolineaModule
   ],
   bootstrap: [AppComponent],
 })
